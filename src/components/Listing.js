218f20b005d0bd6952ee6370d8ff5194c8916645
Leon Si <leonzalion@gmail.com>
2020-02-08 17:16:37 -0500

    oe

---
+++ src/components/Listing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Listing() {
+  return (
+    <div className="listing">
+
+    </div>
+  );
+}
+
+export default Listing;
+
--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function Listings() {
-
   return (
     <div className="listings">
       <ul>
