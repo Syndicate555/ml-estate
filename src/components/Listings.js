c0d259924e700eca6d9129087b557ea1167492ae
Adil <aughr063@uottawa.ca>
2020-02-09 09:46:30 -0500

    Added house list

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import house from "./house_item.png";
+
 
 function Listings() {
   return (
     <div className="listings">
-      <ul>
-        <li>Listing 1</li>
-        <li>Listing 2</li>
-        <li>Listing 3</li>
-      </ul>
+      <div className="service-list">
+        <a href="http://localhost:3000/"><img src={house} alt="icon" className="alignnone size-full wp-image-156 listing-image" /></a>
+
+        <div>
+        <h1>15924 97th St</h1>
+        <h3>$799,000</h3>
+        </div>
+    </div>
+
+    <div className="service-list">
+        <a href="http://localhost:3000/"><img src={house} alt="icon" className="alignnone size-full wp-image-156 listing-image" /></a>
+
+        <div>
+        <h1>1670 Longfellow Ave</h1>
+        <h3>$140,000</h3>
+        </div>
+    </div>
     </div>
   );
 }
