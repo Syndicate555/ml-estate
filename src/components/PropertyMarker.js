d5baa536aefd99a4f33c56a6817500e05b29734f
Leon Si <leonzalion@gmail.com>
2020-02-08 20:30:57 -0500

    idk

--- src/components/PropertyMarker.js
+++ src/components/PropertyMarker.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
-import {Marker} from 'google-map-react';
+import Marker from 'google-map-react';
 
-function PropertyMarker() {
+// Property Marker should have:
+// Longitude, Latitude
+// Exact Address
+// Price
+// Image?
+
+function PropertyMarker(props) {
+  const {lng, lat, address, price, imageURL} = props;
 
   function onMarkerClick(event) {
-
+    console.log(lng, lat, address, price, imageURL);
   }
+
   return (
-    <Marker/>
-  )
+    <Marker
+      onClick={onMarkerClick}
+      position={{lat: lat, lng: lng}}
+    />
+  );
 }
 
 export default PropertyMarker;
